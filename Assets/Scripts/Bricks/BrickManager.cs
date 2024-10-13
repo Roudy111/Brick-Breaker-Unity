b335781bc23d89bf4a51ccc44a3ac73017dba342
Siamak Roudyani <roudyani00@gmail.com>
2024-10-13 20:23:26 +0200

    BrickRegular variation.
    -added 3 variation of  BrickRegular variation with different colors

--- Assets/Scripts/Bricks/BrickManager.cs
+++ Assets/Scripts/Bricks/BrickManager.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BrickManager : MonoBehaviour
 {
-    [SerializeField] private RegularBrick regularBrickPrefab;
+
+    [SerializeField] private RegularBrick[] brickPrebafs;
     [SerializeField] private ExplodingBrick explodingBrickPrefab;
     [SerializeField] private int LineCount = 6;
     [SerializeField][Range(0f, 1f)] private float explodingBrickProbability = 0.2f;
@@ -34,6 +36,22 @@ public class BrickManager : MonoBehaviour
         m_TotalBrick = 0;
         int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
 
+        // Filter out null prefabs
+        List<RegularBrick> validPrefabs = new List<RegularBrick>();
+        foreach (var prefab in brickPrebafs)
+        {
+            if (prefab != null)
+            {
+                validPrefabs.Add(prefab);
+            }
+        }
+
+        if (validPrefabs.Count == 0)
+        {
+            Debug.LogError("No valid brick prefabs found. Please assign prefabs in the inspector.");
+            return;
+        }
+
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -41,20 +59,29 @@ public class BrickManager : MonoBehaviour
                 Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
                 Brick brick;
 
-                if (UnityEngine.Random.value < explodingBrickProbability)
+                if (UnityEngine.Random.value < explodingBrickProbability && explodingBrickPrefab != null)
                 {
                     brick = Instantiate(explodingBrickPrefab, position, Quaternion.identity);
                 }
                 else
                 {
-                    brick = Instantiate(regularBrickPrefab, position, Quaternion.identity);
+                    int randomPrefab = UnityEngine.Random.Range(0, validPrefabs.Count);
+                    brick = Instantiate(validPrefabs[randomPrefab], position, Quaternion.identity);
                 }
 
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed += AddPoint;
-                m_TotalBrick++;
+                if (brick != null)
+                {
+                    brick.PointValue = pointCountArray[i];
+                    brick.onDestroyed += AddPoint;
+                    m_TotalBrick++;
+                }
+                else
+                {
+                    Debug.LogWarning($"Failed to instantiate brick at position {position}");
+                }
             }
         }
+
         Debug.Log($"Total Bricks: {m_TotalBrick}");
     }
 
